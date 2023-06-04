843d3de2f5fdc688a22e6be985c6031cda7f55fa
bj4rnee <bjarne.klar@web.de>
2023-06-04 14:11:01 +0200

    claimer should work now

--- role.claimer.js
+++ role.claimer.js
@@ -1,31 +1,33 @@
 var roleClaimer = {
     /** @param {Creep} creep **/
     run: function (creep) {
-      // If the current room is not claimed
-      if (!creep.room.controller || !creep.room.controller.my) {
-        if (creep.claimController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-          creep.moveTo(creep.room.controller);
-        }
-      } else {
-        // If the current room is claimed
-        var flags = Game.flags;
-  
-        if (Object.keys(flags).length > 0) {
-          // Move towards the nearest visible flag
-          var visibleFlags = Object.values(flags).filter((flag) => Game.rooms[flag.pos.roomName]);
-          var targetFlag = creep.pos.findClosestByRange(visibleFlags);
-  
-          if (targetFlag) {
-            if (creep.pos.isEqualTo(targetFlag.pos)) {
-              // If the creep is already at the flag position, remove the flag and move to the next flag
-              targetFlag.remove();
-            } else {
-              creep.moveTo(targetFlag);
+        // If the current room is not claimed
+        if (!creep.room.controller || !creep.room.controller.my) {
+            if (creep.claimController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
             }
-          }
+        } else {
+            // If the current room is claimed
+            var flags = Game.flags;
+            var targetFlag = null;
+            for (var flagName in flags) {
+                var flag = flags[flagName];
+                if (Game.rooms[flag.pos.roomName]) { // !flag.room.controller
+                    targetFlag = flag;
+                    break;
+                }
+            }
+
+            if (targetFlag) {
+                if (creep.pos.isEqualTo(targetFlag.pos)) {
+                    // If the creep is already at the flag position, remove the flag and move to the next flag
+                    targetFlag.remove();
+                } else {
+                    creep.moveTo(targetFlag);
+                }
+            }
         }
-      }
     },
-  };
-  
-  module.exports = roleClaimer;
+};
+
+module.exports = roleClaimer;
